d70a07311801e4d8ffdc4510177198ba0d5f3826
YashVerma <yashgoldsmith37@gmail.com>
2020-05-22 11:04:40 +0530

    inital commit file

---
+++ Hellowowrld.java
@@ -0,0 +1,9 @@
+public class Hellowowrld
+{
+	public static void main(String args[])
+{
+
+System.out.println("Hello Worrld");
+}
+
+}
---
+++ forloop.java
@@ -0,0 +1,9 @@
+public class Hellowowrld
+{
+	public static void main(String args[])
+{
+for()
+System.out.println("Hello Worrld");
+}
+
+}
